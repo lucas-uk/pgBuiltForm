bcaf29af0eed7fb01aae0f1ab92effd1a9759a2b
Julian Rosser <maptopixel@users.noreply.github.com>
2018-07-13 10:03:58 +0100

    updated built-form classification

--- sql/built_form_classification.sql
+++ sql/built_form_classification.sql
@@ -3,10 +3,38 @@
 -----------------------------------------------------------
 
 
+-- create function to test for existance of fields before adding new ones
+CREATE OR REPLACE function f_add_col(
+   _tbl regclass, _col  text, _type regtype, OUT success bool)
+    LANGUAGE plpgsql AS
+$func$
+BEGIN
+
+IF EXISTS (
+   SELECT 1 FROM pg_attribute
+   WHERE  attrelid = _tbl
+   AND    attname = _col
+   AND    NOT attisdropped) THEN
+   success := FALSE;
+
+ELSE
+   EXECUTE '
+   ALTER TABLE ' || _tbl || ' ADD COLUMN ' || quote_ident(_col) || ' ' || _type;
+   success := TRUE;
+END IF;
+
+END
+$func$;
+
+
 SELECT f_add_col('derivedinfo.buildingsandheights', 'abp_cnt_toid_crossreference', 'bigint');
 SELECT f_add_col('derivedinfo.buildingsandheights', 'abp_cnt_spatially_within', 'bigint');
+SELECT f_add_col('derivedinfo.buildingsandheights', 'abp_cnt_spatially_within_non_add', 'bigint');
 SELECT f_add_col('derivedinfo.buildingsandheights', 'count_st_touch_addresses', 'bigint');
 SELECT f_add_col('derivedinfo.buildingsandheights', 'form_based_on_addressgeometry', 'varchar(25)');
+SELECT f_add_col('derivedinfo.buildingsandheights', 'abp_cnt_spatial_rpc_only', 'bigint');
+SELECT f_add_col('derivedinfo.buildingsandheights', 'abp_cnt_spatial_postal_rpc', 'bigint');
+SELECT f_add_col('derivedinfo.buildingsandheights', 'abp_cnt_spatially_within_old', 'bigint');
 
 -- update abp_cnt_toid_crossreference based on TOID
 UPDATE 
@@ -19,17 +47,104 @@ UPDATE
 			os_topo_toid,
 				count(*) abp_count
 			from
-				os_addressbase.addressbaseplus
+				os_addressbase.addressbaseplus abp
+             where
+                 	(abp.RPC < 3)
+                    and 
+                    (abp.STATE = 2
+                    or abp.STATE = 3
+                    or abp.STATE = 5
+                    or abp.STATE is null)                   
+                    and 
+                    (abp.ADDRESSBASE_POSTAL <> 'M'
+                    and abp.ADDRESSBASE_POSTAL <> 'N' )    
+                    
 			group by
 				os_topo_toid
 		) AS cnt
 	WHERE cnt.os_topo_toid = bld.fid;
 	
+    
+    
+    
+    
+    	
 -- update abp_cnt_spatially_within based on spatial relationship
 UPDATE 
 	derivedinfo.buildingsandheights bld
 	SET    
 		abp_cnt_spatially_within = cnt.abp_count 
+	FROM  
+		(
+		select
+			bld.ogc_fid,
+			count(*) abp_count
+			from 
+				derivedinfo.buildingsandheights bld,
+				os_addressbase.addressbaseplus abp
+			where 
+				st_intersects
+					(
+					bld.wkb_geometry,
+					abp.geometry
+					)            
+           			and
+                 	(abp.RPC < 3)
+                    and 
+                    (abp.STATE = 2
+                    or abp.STATE = 3
+                    or abp.STATE = 5
+                    or abp.STATE is null)                   
+                    and 
+                    (abp.ADDRESSBASE_POSTAL <> 'M'
+                    and abp.ADDRESSBASE_POSTAL <> 'N' )          
+			group by
+				bld.ogc_fid
+		) AS cnt
+	WHERE cnt.ogc_fid = bld.ogc_fid;
+
+
+
+
+-- update abp_cnt_spatially_within_non_add based on spatial relationship
+UPDATE 
+	derivedinfo.buildingsandheights bld
+	SET    
+		abp_cnt_spatially_within_non_add = cnt.abp_count 
+	FROM  
+		(
+		select
+			bld.ogc_fid,
+			count(*) abp_count
+			from 
+				derivedinfo.buildingsandheights bld,
+				os_addressbase.addressbaseplus abp
+			where 
+				st_intersects
+					(
+					bld.wkb_geometry,
+					abp.geometry
+					)            
+           			and
+                 	(abp.RPC < 3)
+                    and 
+                    (abp.STATE = 2
+                    or abp.STATE = 3
+                    or abp.STATE = 5
+                    or abp.STATE is null)      
+			group by
+				bld.ogc_fid
+		) AS cnt
+	WHERE cnt.ogc_fid = bld.ogc_fid;
+    
+
+
+
+-- update abp_cnt_spatially_within_old based on spatial relationship
+UPDATE 
+	derivedinfo.buildingsandheights bld
+	SET    
+		abp_cnt_spatially_within_old = cnt.abp_count 
 	FROM  
 		(
 		select
@@ -44,17 +159,79 @@ UPDATE
 					bld.wkb_geometry,
 					abp.geometry
 					)
-				and abp.RPC<3
+				and abp.RPC < 3
 				and abp.STATE <> 1
 				and abp.STATE <> 4
 				and abp.STATE <> 6
-				and abp.STATE is not null
-				and abp.POSTAL_ADDRESS <> 'M'
-				and abp.POSTAL_ADDRESS <> 'N'
+                and abp.STATE is not null
+                and abp.ADDRESSBASE_POSTAL <> 'M'
+                and abp.ADDRESSBASE_POSTAL <> 'N'
 			group by
 				bld.ogc_fid
 		) AS cnt
 	WHERE cnt.ogc_fid = bld.ogc_fid;
+
+
+
+
+-- update abp_cnt_spatial_rpc_only based on spatial relationship
+UPDATE 
+	derivedinfo.buildingsandheights bld
+	SET    
+		abp_cnt_spatial_rpc_only = cnt.abp_count 
+	FROM  
+		(
+		select
+			bld.ogc_fid,
+				count(*) abp_count
+			from 
+				derivedinfo.buildingsandheights bld,
+				os_addressbase.addressbaseplus abp
+			where 
+				st_intersects
+					(
+					bld.wkb_geometry,
+					abp.geometry
+					)
+                and abp.RPC < 3
+			group by
+				bld.ogc_fid
+		) AS cnt
+	WHERE cnt.ogc_fid = bld.ogc_fid;
+
+
+
+-- update abp_cnt_spatial_postal_rpc based on spatial relationship
+UPDATE 
+	derivedinfo.buildingsandheights bld
+	SET    
+		abp_cnt_spatial_postal_rpc = cnt.abp_count 
+	FROM  
+		(
+		select
+			bld.ogc_fid,
+				count(*) abp_count
+			from 
+				derivedinfo.buildingsandheights bld,
+				os_addressbase.addressbaseplus abp
+			where 
+				st_intersects
+					(
+					bld.wkb_geometry,
+					abp.geometry
+					)
+                and abp.RPC < 3
+                and abp.ADDRESSBASE_POSTAL <> 'M'
+                and abp.ADDRESSBASE_POSTAL <> 'N'
+			group by
+				bld.ogc_fid
+		) AS cnt
+	WHERE cnt.ogc_fid = bld.ogc_fid;
+
+
+
+
+
 
 -- identify the number of addressable buildings touched by other addressable buidlings:
 
@@ -162,3 +339,5 @@ UPDATE
 		form_based_on_addressgeometry = 'complex'
 	WHERE 
 		count_st_touch_addresses > 2;
+
+
